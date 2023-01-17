cb8a8c5e8dd7e68a56cd9a64c6449535e739fb03
Nisarg <nisargpatel1206@gmail.com>
2023-01-17 12:34:27 +0530

    add more

--- questionLinkedList/concatenatingLL.cpp
+++ questionLinkedList/concatenatingLL.cpp
@@ -5,9 +5,9 @@ class node
     public:
         int data;
         node *NAdd;
-} *head = NULL, *tail = NULL;
+} *head1 = NULL,*head2=NULL,*head3=NULL,*tail2=NULL,*tail3=NULL, *tail1 = NULL;
 
-void createLL()
+void createLL1()
 {
         int t = 1,d;
         while(t)
@@ -17,22 +17,107 @@ void createLL()
             cin >> d;
             p->data = d;
             p->NAdd=NULL;
-            if(head==NULL)
+            if(head1==NULL)
             {
-                head = p;
-                tail = p;
+                head1 = p;
+                tail1 = p;
             }
             else{
-                tail->NAdd = p;
-                tail = tail->NAdd;
+                tail1->NAdd = p;
+                tail1= tail1->NAdd;
             }
             cout << "Do you want to add more node" << endl;
             cin >> t;
         }
 }
 
-void printLL()
+
+void createLL2()
 {
-        node *p;
+        int t = 1,d;
+        while(t)
+        {
+            cout << "Enter data " << endl;
+            node *p = new node();
+            cin >> d;
+            p->data = d;
+            p->NAdd=NULL;
+            if(head2==NULL)
+            {
+                head2 = p;
+                tail2 = p;
+            }
+            else{
+                tail2->NAdd = p;
+                tail2 = tail2->NAdd;
+            }
+            cout << "Do you want to add more node" << endl;
+            cin >> t;
+        }
+}
+void createLL3()
+{
+        int t = 1,d;
+        while(t)
+        {
+            cout << "Enter data " << endl;
+            node *p = new node();
+            cin >> d;
+            p->data = d;
+            p->NAdd=NULL;
+            if(head3==NULL)
+            {
+                head3 = p;
+                tail3 = p;
+            }
+            else{
+                tail3->NAdd = p;
+                tail3 = tail3->NAdd;
+            }
+            cout << "Do you want to add more node" << endl;
+            cin >> t;
+        }
+}
+
+
+void printLL(node *p)
+{
         cout << "Linked list is " << endl;
+        while(p!=NULL)
+        {
+            cout << p->data << endl;
+            p = p->NAdd;
+        }
+}
+
+void concatenatingLL(node *p,node *q,node *r)
+{
+    while (p->NAdd!=NULL)
+    {
+            p = p->NAdd;
+    }
+    while (q->NAdd!=NULL)
+    {
+            p->NAdd = q;
+            p = q;
+            q = q->NAdd;
+    }
+    while (r->NAdd!=NULL)
+    {
+            p->NAdd = r;
+            p = r;
+            r = r->NAdd;
+    }
+}
+
+int main()
+{
+    createLL1();
+    printLL(head1);
+    createLL2();
+    printLL(head2);
+    createLL3();
+    printLL(head3);
+    concatenatingLL(head1,head2,head3);
+    printLL(head1);
 }
