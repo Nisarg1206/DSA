fa24cdd40f9ab03f3b0df6a774a06f0ae76008ea
Nisarg <nisargpatel1206@gmail.com>
2023-01-21 17:20:33 +0530

    add more

---
+++ questionLinkedList/reverseLLinGroupOfK.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class node
+{
+    public:
+    int data;
+    node *NAdd;
+} *head = NULL,*tail=NULL;
+
+void createLL()
+{
+    int t = 1;
+    while(t)
+    {
+        cout << "Enter data" << endl;
+        node *p = new node();
+        
+    }
+}
