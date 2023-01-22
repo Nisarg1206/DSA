3698e40d9e0fa49cef74409ab979ae297001b25f
Nisarg <nisargpatel1206@gmail.com>
2023-01-23 00:02:52 +0530

    add more

--- questionLinkedList/reverseINGroup.cpp
+++ questionLinkedList/reverseINGroup.cpp
@@ -1,31 +1,75 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 class node
 {
-    public:
+public:
     int data;
     node *NAdd;
-}*head=NULL,*tail=NULL;
+} *head = NULL, *tail = NULL;
 
-//Create Linked list
+// Create Linked list
 void createLL()
 {
-    int d,t = 1;
-    while(t)
+    int d, t = 1;
+    while (t)
     {
         cout << "Enter data for node" << endl;
         cin >> d;
         node *p = new node();
-        p->data=d;
+        p->data = d;
         p->NAdd = NULL;
-        if(head==NULL)
+        if (head == NULL)
         {
             head = tail = p;
         }
-        else{
+        else
+        {
             tail->NAdd = p;
             tail = tail->NAdd;
         }
-        cout<<"Do you want to enter more node(0/1)"<<endl;
+        cout << "Do you want to enter more node(0/1)" << endl;
+        cin >> t;
     }
+}
+
+node *reverseLLKGroup(node *head,int k)
+{
+    int i = 0;if(head==NULL)
+    {
+        return NULL;
+    }
+    node *p = NULL, *r = NULL, *q = head;
+    while (q != NULL && i < k)
+    {
+        p = q->NAdd;
+        q->NAdd = r;
+        r = q;
+        q = p;
+        i++;
+    }
+    if(p!=NULL)
+    {
+        head->NAdd = reverseLLKGroup(p, k);
+    }
+    return r;
+}
+
+void printLL(node *p)
+{
+    cout << "Linked list are" << endl;
+    while (p != NULL)
+    {
+        cout << p->data << endl;
+        p = p->NAdd;
+    }
+}
+int main()
+{
+    createLL();
+    printLL(head);
+    int k;
+    cout << "Reverse in group of " << endl;
+    cin >> k;
+    node *z = reverseLLKGroup(head,k);
+    printLL(z);
 }
