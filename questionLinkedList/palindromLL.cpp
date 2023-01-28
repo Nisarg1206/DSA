685187bcd66a8f6524753a91e6705d268fb0a8cf
Nisarg <nisargpatel1206@gmail.com>
2023-01-28 11:57:41 +0530

    add more

---
+++ questionLinkedList/palindromLL.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *NAdd;
+} *head = NULL, *tail = NULL;
+
+void createLL()
+{
+    int t = 1, d;
+    while (t)
+    {
+        cout << "Enter data for node" << endl;
+        cin >> d;
+        node *p = new node();
+        p->data = d;
+        p->NAdd = NULL;
+        if (head == NULL)
+        {
+            head = tail = p;
+        }
+        else
+        {
+            tail->NAdd = p;
+            tail = tail->NAdd;
+        }
+        cout << "Do you want to enter more node (0/1)" << endl;
+        cin >> t;
+    }
+}
+
+class solution
+{
+public:
+    bool CheckPalindrome(node *T)
+    {
+        node *p = T;
+        vector<int> arr;
+        while (p != NULL)
+        {
+            arr.push_back(p->data);
+            p = p->NAdd;
+        }
+        return CheckLL(arr);
+    }
+
+private:
+    bool CheckLL(vector<int> a)
+    {
+        int t = a.size();
+        int c = 0;
+        int b = t - 1;
+        while (c <= b)
+        {
+            if (a[c] != a[b])
+                return 0;
+            c++;
+            b--;
+        }
+        return 1;
+    }
+};
+
+// Printing Linked List
+void printLL(node *p)
+{
+    cout << "Linked List are" << endl;
+    while (p != NULL)
+    {
+        cout << p->data << endl;
+        p = p->NAdd;
+    }
+}
+
+int main()
+{
+    createLL();
+    printLL(head);
+    solution check;
+    int p = check.CheckPalindrome(head);
+    if (p)
+    {
+        cout << "Palindrome Number" << endl;
+    }
+    else
+    {
+        cout << "Not Palindrome Number" << endl;
+    }
+}
