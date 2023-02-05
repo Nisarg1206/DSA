f8d125cebb250b8c717743f1d8998fa29ec75bc9
Nisarg <nisargpatel1206@gmail.com>
2023-02-05 23:44:46 +0530

    add more

--- Sort/Linked List/mergeLL.cpp
+++ Sort/Linked List/mergeLL.cpp
@@ -1,155 +1,169 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node
+class node
 {
 public:
     int data;
-    Node *next;
-} *head = NULL, *tail = NULL;
-
-class Solution
-{
-public:
-    void mergeLL(Node *head, Node *tail)
-    {
-        Node *mid = midInLL(head);
-
-        // Left Side LL
-        mergeLL(head, mid);
-
-        // Right side LL
-        mergeLL(mid->next, tail);
-
-        addMerge(head, tail);
-    }
-
-    // finding mid element in LL
-    Node *midInLL(Node *head)
-    {
-        Node *p = head;
-        Node *q = head;
-
-        while (q != NULL)
-        {
-
-            q = q->next;
-            if (q != NULL)
-            {
-                q = q->next;
-            }
-            else
-            {
-                q = NULL;
-                break;
-            }
-            p = p->next;
-        }
-        return p;
-    }
-
-    // merging LL in sorted way
-
-    void addMerge(Node *head1, Node *tail1)
-    {
-        Node *mid;
-        if (head != tail1)
-        {
-            mid = midInLL(head1);
-        }
-        else
-        {
-            return;
-        }
-
-        Node *p = head1;
-        Node *q = mid->next;
-
-        Node *tail3, *third;
-        while (p != mid->next && q != NULL)
-        {
-            if (p->data < q->data)
-            {
-                if (third == NULL)
-                {
-                    third = tail = p;
-                }
-                else
-                {
-                    tail3->next = p;
-                    tail3 = tail3->next;
-                }
-                p = p->next;
-            }
-            else
-            {
-                if (third == NULL)
-                {
-                    third = tail = q;
-                }
-                else
-                {
-                    tail3->next = q;
-                    tail3 = tail3->next;
-                }
-                q = q->next;
-            }
-        }
-
-        while (p != mid->next)
-        {
-            tail3->next = p;
-            tail3 = tail3->next;
-            p = p->next;
-        }
-        while (q != NULL)
-        {
-            tail3->next = q;
-            tail3 = tail3->next;
-            q = q->next;
-        }
-    }
+    node *next;
 };
 
-void createLinkedList()
+node *createLL()
 {
     int d, t = 1;
-    while (t == 1)
+    node *head = NULL, *tail = NULL;
+    while (t)
     {
-        cout << "Enter value for linked list" << endl;
+        cout << "Enter data for Linked List" << endl;
         cin >> d;
-        Node *p = new Node();
+
+        node *p = new node();
         p->data = d;
         p->next = NULL;
+
         if (head == NULL)
         {
-            head = p;
-            tail = p;
+            head = tail = p;
         }
         else
         {
             tail->next = p;
             tail = tail->next;
         }
-        cout << "Do you want to enter more linked list (0/1)" << endl;
+        cout << "do you want to enter more node in LL" << endl;
         cin >> t;
     }
+    return head;
 }
 
-void printLL(Node *p)
+// Printing Linked List
+void printLL(node *p)
 {
-    cout << "Linked List are" << endl;
+    cout << "Linked list are" << endl;
     while (p != NULL)
     {
         cout << p->data << endl;
+        p = p->next;
     }
 }
 
+// Solution
+class Solution
+{
+public:
+    node *mergeSortLL(node *&head)
+    {
+        if (head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+
+        node *mid = findMid(head);
+
+        node *left = head;
+        node *right = mid->next;
+        mid->next = NULL;
+
+        left = mergeSortLL(left);
+        right = mergeSortLL(right);
+
+        node *result = merge(left, right);
+
+        return result;
+    }
+
+    node *findMid(node *p)
+    {
+        node *slow = p;
+        node *fast = p;
+
+        while (fast != NULL)
+        {
+            fast = fast->next;
+            if (fast ->next!= NULL)
+            {
+                fast = fast->next;
+            }
+            else
+            {
+                fast = NULL;
+                break;
+            }
+            slow = slow->next;
+        }
+        return slow;
+    }
+
+    node *merge(node *left, node *right)
+    {
+        if (left == NULL)
+            return right;
+        if (right == NULL)
+            return left;
+
+        node *second = NULL;
+        node *temp;
+
+        while (left != NULL && right != NULL)
+        {
+            if (left->data < right->data)
+            {
+                if (second == NULL)
+                {
+                    second = temp = left;
+                    left=left->next;
+                }
+                else
+                {
+                    temp->next = left;
+                    temp = temp->next;
+                    left = left->next;
+                }
+            }
+            else
+            {
+                if (second == NULL)
+                {
+                    second = temp = right;
+                    right=right->next;
+                }
+                else
+                {
+                    temp->next = right;
+                    temp = temp->next;
+                    right = right->next;
+                }
+            }
+        }
+
+        while (left != NULL)
+        {
+            temp->next = left;
+            temp = temp->next;
+            left = left->next;
+        }
+
+        while (right != NULL)
+        {
+            temp->next = right;
+            temp = temp->next;
+            right = right->next;
+        }
+
+        return second;
+    }
+};
+
 int main()
 {
-    createLinkedList();
-    cout << "Before" << endl;
-    printLL(head);
+    // Cretaing LL
+    node *p = createLL();
+
+    // printing LL
+    printLL(p);
+
     Solution obj;
-    obj.mergeLL(head, tail);
+    node *q = obj.mergeSortLL(p);
+
+    printLL(q);
 }
